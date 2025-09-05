6a50dcda48197b8113454fbfe15a0c0a27409862
Olli Tietäväinen <ollit@vaadin.com>
2025-09-05 11:01:35 +0300

    image example

--- src/main/java/org/vaadin/base/ui/MainView.java
+++ src/main/java/org/vaadin/base/ui/MainView.java
@@ -1,5 +1,10 @@
 package org.vaadin.base.ui;
 
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.server.streams.DownloadHandler;
+import com.vaadin.flow.server.streams.FileDownloadHandler;
 import org.vaadin.base.ui.component.ViewToolbar;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Main;
@@ -8,6 +13,8 @@ import com.vaadin.flow.component.icon.SvgIcon;
 import com.vaadin.flow.router.Menu;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * This view shows up when a user navigates to the root ('/') of the application.
@@ -15,6 +22,7 @@ import com.vaadin.flow.theme.lumo.LumoUtility;
 @Route
 @Menu(order = -100, icon = "vaadin:home", title = "Welcome!")
 public final class MainView extends Main {
+    private final Image image;
 
     // TODO Replace with your own main view.
 
@@ -27,17 +35,34 @@ public final class MainView extends Main {
         contentDiv.addClassNames(LumoUtility.Flex.GROW, LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN,
                 LumoUtility.AlignItems.CENTER, LumoUtility.JustifyContent.CENTER);
 
-        var icon = new SvgIcon("icons/construction.svg");
-        icon.addClassNames(LumoUtility.TextColor.SUCCESS);
-        icon.getStyle().setWidth("200px");
-        icon.getStyle().setHeight("200px");
 
-        var centerDiv = new Div(icon, new Paragraph("Replace this view with your own main view!"));
+        var centerDiv = new Div();
         centerDiv.addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN,
                 LumoUtility.AlignItems.CENTER);
+
+        image = new Image();
+        image.setWidth(300, Unit.PIXELS);
+        image.setHeight(450, Unit.PIXELS);
+        File file = new File(".\\src\\main\\resources\\META-INF\\resources\\images\\test.png");
+        FileDownloadHandler fileDownloadHandler = DownloadHandler.forFile(file);
+        image.setSrc(fileDownloadHandler);
+        image.addClickListener(e -> refreshImage());
+        centerDiv.add(image);
         contentDiv.add(centerDiv);
 
         add(new ViewToolbar("Welcome to Vaadin!"));
         add(contentDiv);
+    }
+
+    private void refreshImage() {
+        File file = new File(".\\src\\main\\resources\\META-INF\\resources\\images\\test.png");
+        FileDownloadHandler fileDownloadHandler = DownloadHandler.forFile(file);
+        image.setSrc(fileDownloadHandler);;
+        try {
+            Notification.show("Showing " + file.getCanonicalPath());
+        } catch (IOException io) {
+            Notification.show("IOException: " + io.getMessage());
+        }
+
     }
 }
